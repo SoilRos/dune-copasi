84d4d4a8cacda49b19a39334e4f3b1a3a0c04e84
Santiago Ospina De Los Rios <santiago.ospina@iwr.uni-heidelberg.de>
2019-09-17 17:31:26 +0200

    call right files in gitlabCI & check AppVeyor falure

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,4 +1,6 @@
 # build script for all CIs
 
 echo "DUNE_OPTIONS_FILE: ${DUNE_OPTIONS_FILE}"
-./dune-common/bin/dunecontrol --opts=${DUNE_OPTIONS_FILE} --module=dune-copasi all
+echo "DUNECONTROL: ${DUNECONTROL}"
+
+${DUNECONTROL} --opts=${DUNE_OPTIONS_FILE} --module=dune-copasi all
