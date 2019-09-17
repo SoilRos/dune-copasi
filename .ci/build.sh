a6fff9c0d929cdb5be86488815220f69559526d9
Santiago Ospina De Los Rios <santiago.ospina@iwr.uni-heidelberg.de>
2019-09-17 17:39:21 +0200

    Only build dune-copasi on build stage

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,4 +3,4 @@
 echo "DUNE_OPTIONS_FILE: ${DUNE_OPTIONS_FILE}"
 echo "DUNECONTROL: ${DUNECONTROL}"
 
-${DUNECONTROL} --opts=${DUNE_OPTIONS_FILE} --module=dune-copasi all
+${DUNECONTROL} --opts=${DUNE_OPTIONS_FILE} --only=dune-copasi all
